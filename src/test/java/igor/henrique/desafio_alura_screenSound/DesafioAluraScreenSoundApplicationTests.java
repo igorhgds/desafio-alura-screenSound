894a08882f12177e3e19be2affa2c70a6c9865df
Igor Henrique <61789282+igorhgds@users.noreply.github.com>
2025-03-26 16:46:14 -0300

    subindo projeto base com spring initializr

---
+++ src/test/java/igor/henrique/desafio_alura_screenSound/DesafioAluraScreenSoundApplicationTests.java
@@ -0,0 +1,13 @@
+package igor.henrique.desafio_alura_screenSound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAluraScreenSoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
