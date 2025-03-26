729fe7d8c6bd52f0ab5f70ca3d2a82a444eac7ee
Igor Henrique <61789282+igorhgds@users.noreply.github.com>
2025-03-26 16:59:09 -0300

    create Principal Class with exibeMenu method

--- src/main/java/igor/henrique/desafio_alura_screenSound/DesafioAluraScreenSoundApplication.java
+++ src/main/java/igor/henrique/desafio_alura_screenSound/DesafioAluraScreenSoundApplication.java
@@ -1,13 +1,21 @@
 package igor.henrique.desafio_alura_screenSound;
 
+import igor.henrique.desafio_alura_screenSound.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioAluraScreenSoundApplication {
+public class DesafioAluraScreenSoundApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioAluraScreenSoundApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Desafio Alura Screen Sound Application");
+		Principal principal = new Principal();
+		principal.exibeMenu();
+	}
 }
